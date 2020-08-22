9360e9592b57d0272c2283cba6387bb95f0d33ab
hossamhasanin <kokojake13@gmail.com>
2020-08-22 07:53:08 +0200

    fix the sign up and login , update the design of users fragment

--- function/functions/index.js
+++ function/functions/index.js
@@ -6,9 +6,33 @@ const db = admin.firestore();
 exports.leavingRoomListener = functions.firestore.document("rooms/{room_id}/users/{user_id}")
     .onDelete((snap, context) => {
         if (snap.data()["leader"]) {
-            return changeLeader(context)
+            changeLeader(context)
         }
-        return
+
+        const notificationTitle = "There is someone left !"
+        const notificationBody = snap.data().name + " has left"
+
+
+        const payload = {
+            notification: {
+                title: notificationTitle,
+                body: notificationBody,
+                icon: "default"
+            },
+            data: {
+                noti_type: "userState",
+                user_id: snap.data().id
+            }
+        }
+
+        const goesTo = "userState";
+
+        return admin.messaging().sendToTopic(goesTo, payload).then(result => {
+            console.log("Notification sent successfully " + goesTo);
+        }).catch(err => {
+            console.log("Error : " + err);
+        });
+
     });
 
 async function changeLeader(context) {
@@ -55,7 +79,8 @@ async function sendInvitation(snap) {
             sendedBy_name: sendedByName,
             room_desc: snap.data().desc,
             room_name: snap.data().name,
-            room_id: snap.data().id
+            room_id: snap.data().id,
+            room_video_url: snap.data().mp4Url
         }
     }
 
@@ -65,4 +90,87 @@ async function sendInvitation(snap) {
     }).catch(err => {
         console.log("Error : " + err);
     });
-}    
+}
+
+exports.notifyAboutUserStateUpdate = functions.firestore.document("rooms/{room_id}/users/{user_id}")
+    .onUpdate((change, context) => {
+        const oldData = change.before.data();
+        const newData = change.after.data();
+        const states = {
+            ready: 0,
+            playing: 1,
+            finished: 2,
+            stop: 3
+        }
+
+        let state = newData.state
+
+        let notificationTitle = "";
+        let notificationBody = "";
+        let payload = {
+            notification: {
+                title: notificationTitle,
+                body: notificationBody,
+                icon: "default"
+            },
+            data: {
+                noti_type: "userState"
+            }
+        };
+        let goesTo = "userState";
+
+        if (state == states.stop) {
+
+            notificationTitle = "There is someone stopped the video"
+            notificationBody = newData.name + " has stopped the video"
+
+
+            payload = {
+                notification: {
+                    title: notificationTitle,
+                    body: notificationBody,
+                    icon: "default"
+                },
+                data: {
+                    noti_type: "userState",
+                    user_id: newData.id
+                }
+            }
+
+            return admin.messaging().sendToTopic(goesTo, payload).then(result => {
+                console.log("Notification sent successfully " + goesTo);
+            }).catch(err => {
+                console.log("Error : " + err);
+            });
+        } else if (state == states.playing) {
+            if (oldData.state == states.stop) {
+
+                notificationTitle = "Started Playing again !"
+                notificationBody = newData.name + " is continuing the video"
+
+                payload = {
+                    notification: {
+                        title: notificationTitle,
+                        body: notificationBody,
+                        icon: "default"
+                    },
+                    data: {
+                        noti_type: "userState",
+                        user_id: newData.id
+                    }
+                }
+
+                return admin.messaging().sendToTopic(goesTo, payload).then(result => {
+                    console.log("Notification sent successfully " + goesTo);
+                }).catch(err => {
+                    console.log("Error : " + err);
+                });
+
+            } else {
+                return
+            }
+        } else {
+            return
+        }
+
+    });
